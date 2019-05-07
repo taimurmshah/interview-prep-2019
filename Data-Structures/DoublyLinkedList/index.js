90d0810090510f393bdf84e9d67b3513e655d762
github username <github email address>
2019-05-06 20:45:57 -0400

    dll unshift

--- Data-Structures/DoublyLinkedList/index.js
+++ Data-Structures/DoublyLinkedList/index.js
@@ -59,6 +59,20 @@ class DoublyLinkedList {
     this.length--;
     return shifted;
   }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.previous = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let first = new Node(13);
