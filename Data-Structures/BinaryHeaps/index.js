4e42b1d0931459f0fc5d197d1146bf30c39c4ac1
github username <github email address>
2019-05-27 18:40:24 -0400

    MaxBinaryHeap, insert method done

---
+++ Data-Structures/BinaryHeaps/index.js
@@ -0,0 +1,41 @@
+//Binary Heaps
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+
+  getParent(index) {
+    let newIndex = Math.floor((index - 1) / 2);
+
+    return newIndex;
+  }
+
+  insert(value) {
+    // debugger;
+    this.values.push(value);
+    if (this.values.length === 1) return this;
+    let childIndex = this.values.length - 1;
+    let parentIndex = this.getParent(childIndex);
+    while (this.values[childIndex] > this.values[parentIndex]) {
+      // debugger
+      [this.values[childIndex], this.values[parentIndex]] = [
+        this.values[parentIndex],
+        this.values[childIndex]
+      ];
+      childIndex = parentIndex;
+      parentIndex = this.getParent(childIndex);
+    }
+    return this.values;
+  }
+}
+
+let maxHeap = new MaxBinaryHeap();
+
+maxHeap.insert(100);
+maxHeap.insert(32);
+maxHeap.insert(23);
+// maxHeap.insert(17);
+// maxHeap.insert(31);
+// maxHeap.insert(2);
+// maxHeap.insert(21);
