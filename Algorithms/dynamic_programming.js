95a3fa6bdc535ab31939c38776deeb5aad521399
github username <github email address>
2019-06-02 17:14:08 -0400

    finished algo course

--- Algorithms/dynamic_programming.js
+++ Algorithms/dynamic_programming.js
@@ -9,12 +9,24 @@ let fibonacci = n => {
 // the cached result when the same inputs
 // occur again
 
-let betterFib = (n, memo = []) => {
+//roughly O(n)
+//huge space-complexity
+let memoFib = (n, memo = []) => {
   if (memo[n] !== undefined) {
     return memo[n];
   }
   if (n <= 2) return 1;
-  let res = betterFib(n - 1, memo) + betterFib(n - 2, memo);
+  let res = memoFib(n - 1, memo) + memoFib(n - 2, memo);
   memo[n] = res;
   return res;
+};
+
+//also roughly O(n) -- one loop
+let tabFib = n => {
+  if (n <= 2) return 1;
+  let fibNums = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+  }
+  return fibNums[n];
 };
