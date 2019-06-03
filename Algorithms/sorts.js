8395a1400dd6f49e85752aaf2b3135e3b90124f0
github username <github email address>
2019-06-03 13:27:49 -0400

    working on sorts

--- Algorithms/sorts.js
+++ Algorithms/sorts.js
@@ -1,29 +1,20 @@
+/* Bubble sort is a sorting algorithm in which the largest value
+ * per each iteration bubbles to the end of the array, in place.
+ * the way it works is that two elements are constantly being compared:
+ * the element at index j, and the element at index j+1. Each loop, the
+ * largest value will by definition become the final value in the array;
+ * after each loop, the array-length over which the loop is running will
+ * decrement by one, starting at array.length. if the conditional in the
+ * nested loop runs, the boolean variable noSwaps is set to false, which
+ * forces the loop to run again.*/
 let bubbleSort = array => {
   let noSwaps;
   for (let i = array.length; i > 0; i--) {
     noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
-      console.log("one pass through");
       if (array[j] > array[j + 1]) {
-        noSwaps = false;
-        let temp = array[j + 1];
-        array[j + 1] = array[j];
-        array[j] = temp;
-      }
-    }
-    if (noSwaps) break;
-  }
-  return array;
-};
-
-let bubbleSort2 = array => {
-  let noSwaps;
-  for (let i = array.length; i >= 0; i--) {
-    noSwaps = true;
-    for (let j = 0; j < i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        noSwaps = false;
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        noSwaps = false;
       }
     }
     if (noSwaps) break;
