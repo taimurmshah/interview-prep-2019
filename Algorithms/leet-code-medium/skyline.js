57d09ba2b2b7ac8c0fc9755e319bbb4d44d0c94a
github username <taimurmshah@gmail.com>
2019-08-22 03:05:26 -0400

    what a beautiful algorithm

---
+++ Algorithms/leet-code-medium/skyline.js
@@ -0,0 +1,105 @@
+/* Skyline
+ * https://leetcode.com/problems/max-increase-to-keep-city-skyline/
+ * */
+
+//2-d array. the value @ grid[i][j] will be the lesser of the two greatest vertical and horizontal values;
+//if grid[i][j] represents the intersection of the vertical and horizontal axes, its value will be the lesser
+//of the greatest x-axis value and y-axis value.
+//i believe the largest this grid can be is 50x50.
+//i think I can solve this by DFS:
+//  create a variable called "maxSum"
+//  iterate through the grid, and for each value, DFS vertically and horizontally to find the lesser great value
+//  once the value is found, find the difference between it and the current value @ grid[i][j] and increment maxSum by that difference
+//  replace the value @ grid[i][j] with the found value.
+//so, i'll have two variables, x-max && y-max, which will
+
+//the recursive function:
+//what is changing?
+//  indices of the grid.
+//what is the basecase?
+//  when I've gotten to the end of the vertical and horizontal arrays
+//what am I going to return?
+//  the greatest value found in that 1-d search
+//how can I test the values?
+//  feed in the currentValue
+
+let myMaxIncreaseKeepingSkyline = grid => {
+  let maxSum = 0;
+
+  let left = (grid, i, j, xMax) => {
+    if (j < 0) return xMax;
+    xMax = Math.max(xMax, grid[i][j]);
+    return left(grid, i, j - 1, xMax);
+  };
+
+  let right = (grid, i, j, xMax) => {
+    // debugger;
+    if (j >= grid[0].length) return xMax;
+    xMax = Math.max(xMax, grid[i][j]);
+    return right(grid, i, j + 1, xMax);
+  };
+
+  let up = (grid, i, j, yMax) => {
+    if (i < 0) return yMax;
+    yMax = Math.max(yMax, grid[i][j]);
+    return up(grid, i - 1, j, yMax);
+  };
+
+  let down = (grid, i, j, yMax) => {
+    if (i >= grid.length) return yMax;
+    yMax = Math.max(yMax, grid[i][j]);
+    return down(grid, i + 1, j, yMax);
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      let currentValue = grid[i][j];
+
+      let x = Math.max(
+        left(grid, i, j, currentValue),
+        right(grid, i, j, currentValue)
+      );
+      let y = Math.max(
+        up(grid, i, j, currentValue),
+        down(grid, i, j, currentValue)
+      );
+
+      let newValue = Math.min(x, y);
+
+      maxSum += Math.abs(newValue - currentValue);
+    }
+  }
+  return maxSum;
+};
+
+//My solution beat... 5% of other submissions... I guess DFS with recursion is not the best way to do this problem.
+//Here's a solution from Leetcode that beats 95% of JS solutions:
+
+//this is a fucking beautiful algorithm.
+const maxIncreaseKeepingSkyline = grid => {
+  let diff = 0;
+
+  const rows = [];
+  const cols = [];
+  debugger;
+  for (let i = 0; i < grid.length; i++) {
+    rows[i] = 0;
+
+    for (let j = 0; j < grid[i].length; j++) {
+      if (i === 0) cols[j] = 0;
+
+      rows[i] = Math.max(rows[i], grid[i][j]);
+      //the j is really important; the algorithm is finding the vertical and horizontal maximum values.
+      cols[j] = Math.max(cols[j], grid[i][j]);
+    }
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let max = Math.min(rows[i], cols[j]);
+      diff += max - grid[i][j];
+    }
+  }
+
+  return diff;
+};
